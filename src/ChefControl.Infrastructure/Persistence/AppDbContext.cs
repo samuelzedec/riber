3192bd7e554237a5186123316e0164de1d10292c
Samuel Ribeiro <samuel.ribeiro@rotadasoficinas.com.br>
2025-05-31 17:12:21 -0400

    chore: add configurations for Infrastructure layer

---
+++ src/ChefControl.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChefControl.Infrastructure.Persistence;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options)
+    : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        => modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+}
