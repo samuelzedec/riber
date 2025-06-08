e21bf02a6bdf3e8906a9854ed4135a7aba6eef3a
Samuel Ribeiro <samuel.ribeiro@rotadasoficinas.com.br>
2025-06-08 15:38:24 -0400

    refactor: ajusting namespace

--- src/ChefControl.Domain/SharedContext/Abstractions/IRepository.cs
+++ src/ChefControl.Domain/SharedContext/Abstractions/IRepository.cs
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-﻿using ChefControl.Domain.SharedContext.Abstractions;
-
-namespace ChefControl.Domain.SharedContext.Persistence;
-=======
 ﻿namespace ChefControl.Domain.SharedContext.Abstractions;
->>>>>>> 424443c (feat: Add tests for TaxId)
 
 public interface IRepository<T> where T : IAggregateRoot;
