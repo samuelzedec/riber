df62d4b56c28ddc70d3f266f0e90e7e13a4557af
Samuel Ribeiro <samuel.ribeiro@rotadasoficinas.com.br>
2025-06-08 15:31:21 -0400

    feat: Add tests for TaxId
    - Adds new tests and refactoring in VO`s TaxId
    - Change in location of document validators

--- src/ChefControl.Domain/SharedContext/ValueObjects/DocumentValidation/Exceptions/UnsupportedCompanyTypeException.cs
+++
@@ -1,6 +0,0 @@
-using ChefControl.Domain.SharedContext.Exceptions;
-
-namespace ChefControl.Domain.SharedContext.ValueObjects.DocumentValidation.Exceptions;
-
-public class UnsupportedCompanyTypeException(string message) 
-    : DomainException(message);
