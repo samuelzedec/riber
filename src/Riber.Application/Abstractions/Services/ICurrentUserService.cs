a3ed36bb4054178159d2f01c6cdca06911f5cea0
Samuel Zedec <samuel.ribeiro77f@gmail.com>
2025-09-14 21:16:24 -0400

    docs: Add usage remarks and new method `GetCompanyId` to `ICurrentUserService`
    - Added `<remarks>` to indicate proper usage of `ICurrentUserService` in authenticated contexts.
    - Introduced `GetCompanyId` method to retrieve the company identifier for the current user.
    - Removed redundant modifiers from interface method definitions for cleaner syntax.

--- src/Riber.Application/Abstractions/Services/ICurrentUserService.cs
+++ src/Riber.Application/Abstractions/Services/ICurrentUserService.cs
@@ -4,17 +4,27 @@ namespace Riber.Application.Abstractions.Services;
 /// Define um serviço para acessar informações relacionadas ao usuário atual.
 /// Este serviço é normalmente usado para recuperar detalhes como identidade do usuário, funções ou dados da sessão.
 /// </summary>
+/// <remarks>
+/// Este serviço só deve ser utilizado em casos onde a autenticação já foi realizada,
+/// como em controladores ou middlewares que processam requisições autenticadas.
+/// </remarks>
 public interface ICurrentUserService
 {
     /// <summary>
     /// Obtém as permissões associadas ao usuário atual.
     /// </summary>
     /// <returns>Um array de strings representando as permissões do usuário atual.</returns>
-    public string[] GetPermissions();
+    string[] GetPermissions();
 
     /// <summary>
     /// Obtém o identificador único associado ao usuário atual.
     /// </summary>
-    /// <returns>Um objeto Guid opcional representando o identificador do usuário atual ou null se não estiver disponível.</returns>
-    public Guid? GetUserId();
+    /// <returns>Um objeto Guid opcional representando o identificador do usuário atual.</returns>
+    Guid GetUserId();
+
+    /// <summary>
+    /// Obtém o identificador único da empresa associado ao usuário atual.
+    /// </summary>
+    /// <returns>Um objeto Guid representando o identificador da empresa do usuário atual.</returns>
+    Guid GetCompanyId();
 }
