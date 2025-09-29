04157c134056cdfdaf6abcbd727673ac6ec47ad3
Samuel Zedec <samuel.ribeiro77f@gmail.com>
2025-09-29 18:46:14 -0400

    refactor: Remove unused `IsFailure` property from `Result` class

--- src/Riber.Application/Common/Result.cs
+++ src/Riber.Application/Common/Result.cs
@@ -21,7 +21,6 @@ public class Result
     }
 
     public bool IsSuccess { get; }
-    public bool IsFailure => !IsSuccess;
     public Error Error { get; }
 
     public static Result Success() => new(true, Error.None);
