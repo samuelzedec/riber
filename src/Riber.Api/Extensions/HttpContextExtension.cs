2c84d59356db4a41906810cee75a7fa83e6be721
Samuel Zedec <samuel.ribeiro77f@gmail.com>
2025-10-26 21:13:31 -0400

    refactor: melhorar middlewares e extensions da API
    - Atualiza SecurityStampMiddleware
    - Melhora HttpContextExtension e ResultExtension
    - Atualiza GlobalExceptionHandler e BuilderExtension
    - Refatora AuthController

--- src/Riber.Api/Extensions/HttpContextExtension.cs
+++ src/Riber.Api/Extensions/HttpContextExtension.cs
@@ -1,7 +1,7 @@
+using System.Net;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Riber.Application.Common;
-using Riber.Domain.Abstractions;
 
 namespace Riber.Api.Extensions;
 
@@ -15,31 +15,18 @@ public static class HttpContextExtension
 
     public static async Task WriteErrorResponse(
         this HttpContext context,
-        int code,
+        HttpStatusCode code,
         string message,
         Dictionary<string, string[]> details)
     {
-        var errorType = GetErrorCode(code);
         var response = details.Count == 0
-            ? Result.Failure<object>(new Error(errorType, message))
-            : Result.Failure<object>(new Error(errorType, details));
+            ? Result.Failure<object>(message, code)
+            : Result.Failure<object>(details, code);
 
         var jsonResponse = JsonSerializer.Serialize(response, JsonOptions);
-        context.Response.StatusCode = code;
+        context.Response.StatusCode = (int)code;
         context.Response.ContentType = "application/json";
 
         await context.Response.WriteAsync(jsonResponse);
     }
-
-    private static string GetErrorCode(int statusCode) => statusCode switch
-    {
-        400 => "BAD_REQUEST",
-        401 => "UNAUTHORIZED",
-        404 => "NOT_FOUND",
-        408 => "REQUEST_TIMEOUT",
-        409 => "CONFLICT",
-        422 => "UNPROCESSABLE_ENTITY",
-        500 => "INTERNAL_SERVER_ERROR",
-        _ => "APPLICATION_ERROR"
-    };
 }
--- src/Riber.Api/Extensions/ResultExtension.cs
+++ src/Riber.Api/Extensions/ResultExtension.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Riber.Application.Common;
 
@@ -5,6 +6,8 @@ namespace Riber.Api.Extensions;
 
 public static class ResultExtension
 {
-    public static IActionResult ToHttpResult<T>(this Result<T> result)
-        => new ObjectResult(result) { StatusCode = (int)result.StatusCode };
+    public static IActionResult ToHttpResult<T>(this Result<T> result, string location = "")
+        => result.StatusCode is HttpStatusCode.Created
+            ? new CreatedResult(location, result)
+            : new ObjectResult(result) { StatusCode = (int)result.StatusCode };
 }
